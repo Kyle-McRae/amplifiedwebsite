5ff8d3938f4445d07682d2b20d06d40ef8a5d5e3
Kyle-McRae <kyle.mcrae1@gmail.com>
2024-03-09 13:37:39 -0800

    centering and mobile

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -10,8 +10,7 @@ const Blog = () => {
       </div>
       <div className='post'>
         <h2>11/1/2023 - myFirstBlogPost</h2>
-        <p>Welcome to my blog! As of now, this is the only post, but I plan on updating it with fun tech projects or interesting developments in the tech world. My biggest project right now is this website. I've never used AWS amplify and also never handled a full react project solo, so it is proving to be a fun and very educational experience as well. I have a few ideas for the website, but I will have more as I implement them, and I can't wait to see where this takes me. In other news, halloween was yesterday and I carved a very handsome pumpkin for my porch.</p>
-        <img className="blogimg" src="mypumpkin.jpg" alt="pumpkin"></img>
+        <p>Welcome to my blog! As of now, this is the only post, but I plan on updating it with fun tech projects or interesting developments in the tech world. My biggest project right now is this website. I've never used AWS amplify and also never handled a full react project solo, so it is proving to be a fun and very educational experience as well. I have a few ideas for the website, but I will have more as I implement them, and I can't wait to see where this takes me.</p>
       </div>
     </div>
   );
