b54c53544365c76478b5a59c22e99b8c42170bb3
Kyle-McRae <kyle.mcrae1@gmail.com>
2024-03-13 11:55:07 -0700

    fixed grocery deletion button deleting wrong grocery

--- src/components/Grocery/Grocery.js
+++ src/components/Grocery/Grocery.js
@@ -2,25 +2,24 @@ import React from 'react';
 import axios from 'axios';
 import './Grocery.css';
 
-var storedGrocery;
 
-var handleDelete = event => {
-  axios.delete('https://hjj41fkxn9.execute-api.us-west-2.amazonaws.com/dev/groc/' + storedGrocery)
+var handleDelete = grocery => {return  event => {
+  axios.delete('https://hjj41fkxn9.execute-api.us-west-2.amazonaws.com/dev/groc/' + grocery)
     .then(res => {
       console.log(res);
       window.location.reload();
     }).catch(err =>{
       console.log(err);
     });
+  }
 }
 
 export default function Grocery ({grocery}){
 
-  storedGrocery = grocery.Name.S
 
   return(
     <div>
-  <div className="name">{grocery.Name.S}</div><div className="qty">{grocery.Quantity.N}</div><button className="delete" onClick={handleDelete}>X</button>
+  <div className="name">{grocery.Name.S}</div><div className="qty">{grocery.Quantity.N}</div><button className="delete" onClick={handleDelete(grocery.Name.S)}>X</button>
   </div>
   )
 }
