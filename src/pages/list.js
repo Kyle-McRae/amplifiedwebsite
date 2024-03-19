603a775c3b4e93cac577f4ce063a8327eb7cff5e
Kyle-McRae <kyle.mcrae1@gmail.com>
2024-03-19 10:23:01 -0700

    added short grocery list description

--- src/pages/list.js
+++ src/pages/list.js
@@ -3,7 +3,11 @@ import './list.css'
 import GrocList from '../components/Grocery/GrocList';
 const List = () => {
   return (
-    <GrocList/> 
+    <div>
+    <GrocList/>
+    <h2>How it works</h2>
+    <p>This grocery list uses HTTP requests to recieve and send grocery data to my API. This API uses AWS API gateway to recieve requests, a Lambda function to handle the logic, and a DynamoDB table to store the data. When the page is first loaded, the Lambda scans the database for the grocery list, and later additions and deletions are sent to the database while the list is being used. </p>
+    </div>
   );
 };
 
